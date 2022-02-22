08a939f883d5ce0704be2593e78d558376e9d817
Gustavo Bays <gustavo_boliv@hotmail.com>
2022-02-21 21:23:12 -0300

    feat: new insert method e safety changes on the application

---
+++ public/tarefa_controller.php
@@ -0,0 +1,5 @@
+<?php
+
+    include_once '../tarefa_controller.php'
+
+?>
--- tarefa.service.php
+++ tarefa.service.php
@@ -2,8 +2,20 @@
 
 //CRUD
 class TarefaService {
+
+    private $conexao;
+    private $tarefa;
+
+    public function __construct(Conexao $conexao, Tarefa $tarefa) {
+        $this->conexao = $conexao->conectar();
+        $this->tarefa = $tarefa;
+    }
+
     public function inserir() {//create
-    
+        $query = 'INSERT INTO tb_tarefas(tarefa)values(:tarefa)';
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bindValue('tarefa', $this->tarefa->__get('tarefa'));
+        $stmt->execute();
     }
 
 
