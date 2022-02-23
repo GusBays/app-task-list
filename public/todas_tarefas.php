c4f85527315aeb76321db9315f2b67a6c899998a
Gustavo Bays <gustavo_boliv@hotmail.com>
2022-02-22 22:09:20 -0300

    feat: update registers in the database

--- public/todas_tarefas.php
+++ public/todas_tarefas.php
@@ -14,6 +14,56 @@
 		<link rel="stylesheet" href="css/estilo.css">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+	
+		<script>
+			function editar(id, txt_tarefa) {
+				
+				//criar um form para edição
+				let form = document.createElement('form')
+				form.action = './tarefa_controller.php?acao=atualizar'
+				form.method = 'post'
+				form.className = 'row'
+
+				//criar um input para entrada do texto
+				let inputTarefa = document.createElement('input')
+				inputTarefa.type = 'text'
+				inputTarefa.name = 'tarefa'
+				inputTarefa.class = ' col-9 form-control'
+				inputTarefa.value = txt_tarefa
+
+				//criar um input header para guardar o id da tarefa pro back end depois
+				let inputId = document.createElement('input')
+				inputId.type = 'hidden'
+				inputId.name = 'id'
+				inputId.value = id
+
+				//criar um button para enviar os dados pro back-end
+				let button = document.createElement('button')
+				button.type = 'submit'
+				button.className = 'col-3 btn btn-info'
+				button.innerHTML = 'Atualizar'
+
+				//incluir input tarefa no form
+				form.appendChild(inputTarefa)
+
+				//incluir o input header no form
+				form.appendChild(inputId)
+
+				//incluir button no form
+				form.appendChild(button)
+
+				//selecionar a div tarefa com o id recebido do foreach
+				let tarefa = document.getElementById('tarefa_'+id)
+
+				//limpar conteudo interno da div pra sobrepor depois
+				tarefa.innerHTML = ''
+
+				//incluir form na pagina
+				tarefa.insertBefore(form, tarefa[0])
+
+			}
+		</script>
+
 	</head>
 
 	<body>
@@ -45,10 +95,12 @@
 
 								<?php foreach($tarefas as $key => $tarefa) { ?>
 										<div class="row mb-3 d-flex align-items-center tarefa">
-										<div class="col-sm-9"><?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)</div>
+										<div class="col-sm-9" id="tarefa_<?= $tarefa->id ?>">
+											<?= $tarefa->tarefa ?> (<?= $tarefa->status ?>)
+										</div>
 										<div class="col-sm-3 mt-2 d-flex justify-content-between">
 											<i class="fas fa-trash-alt fa-lg text-danger"></i>
-											<i class="fas fa-edit fa-lg text-info"></i>
+											<i class="fas fa-edit fa-lg text-info" onclick="editar(<?= $tarefa->id ?>, '<?= $tarefa->tarefa ?>')"></i>
 											<i class="fas fa-check-square fa-lg text-success"></i>
 										</div>
 										</div>
